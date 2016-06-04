c9720257e23cdf533ccb3a2552db7e8e2f8afc4d
Toby Flemming <tobyf93@hotmail.com>
2016-06-04 14:16:27 +1000

    Sending/receiving messages over network

--- client.js
+++ client.js
@@ -6,14 +6,15 @@ const net = require('net'),
       });
 
 (function() {
-  const port = process.argv[2];
+  const host = process.argv[2];
+  const port = process.argv[3];
 
-  if (!port) {
-    console.error('No Port Specified');
+  if (!host || !port) {
+    console.error('No Server and/or Port Specified');
     return;
   }
 
-  const socket = net.createConnection({ port })
+  const socket = net.createConnection({ host, port })
     .on('connect', () => {
       const address = socket.address();
       console.log('Connected To Server: %j', address);
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const net = require('net');
 
 (function() {
+  var sockets = [];
+
+  const emitMessage = function(message) {
+    sockets.forEach((socket) => {
+      socket.write(message);
+    })
+  }
+
   const server = net.createServer()
     .on('listening', () => {
       const address = server.address();
       console.log('Started server: %j', address);
     })
     .on('connection', (socket) => {
-      socket.on('data', (data) => {
-        // console.log(data.toString());
-        socket.write(data)
+      sockets.push(socket);
+
+      socket.on('data', (message) => {
+        emitMessage(message);
       });
     })
     .on('error', (err) => {
       throw err;
     });
 
-  server.listen();
+  server.listen({
+    host: '0.0.0.0',
+    port: 3000
+  });
 })();
